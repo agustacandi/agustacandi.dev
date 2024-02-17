1e06f3fabe74e4ce9d07fa7a23756ba467dd6040
Candi Agusta <mugiwaraagusta@gmail.com>
2024-02-17 12:05:52 +0700

    feat: add image props

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	export let title: string, description: string;
+	export let title: string, description: string, image: string;
 </script>
 
 <div class="border-2 dark:border-white border-black">
 	<img
-		class="h-28 w-full object-cover"
-		src="https://avatars.githubusercontent.com/u/54197587?v=4"
-		alt="Avatar"
+		class="h-36 w-full object-cover object-top"
+		src={image}
+		alt="Project Preview"
 		loading="lazy"
 	/>
 	<div class="p-2">
-		<h3 class="font-momentum text-lg uppercase line-clamp-1">
+		<h3 class="font-momentum uppercase line-clamp-2">
 			{title}
 		</h3>
 		<p class="font-ibmPlex line-clamp-2">
