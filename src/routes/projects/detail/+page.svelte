f9967cac39c4a4d79c4a514b323bc3d986a51449
Candi Agusta <mugiwaraagusta@gmail.com>
2024-02-17 12:09:13 +0700

    feat: add detail page

---
+++ src/routes/projects/detail/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import MainLayout from '$lib/components/MainLayout.svelte';
+</script>
+
+<MainLayout title="Detail">
+	<h1>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum officia optio earum dolor.
+		Beatae, laudantium.
+	</h1>
+</MainLayout>
