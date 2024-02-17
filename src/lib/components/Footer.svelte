947c52590c04206b61ce5308ed8a9f66d12c3897
Candi Agusta <mugiwaraagusta@gmail.com>
2024-02-17 12:05:29 +0700

    feat: add margin bottom (mobile view)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	const date = new Date();
 </script>
 
-<footer class="flex justify-between font-ibmPlex py-8">
+<footer class="flex justify-between font-ibmPlex py-8 mb-10 md:mb-0">
 	<p>Copyright © {date.getFullYear()}</p>
 	<p>Made with <a class="font-momentum" href="https://kit.svelte.dev">SvelteKit</a></p>
 </footer>
